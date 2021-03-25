487d93ac8ff37fabbe1612bf05f8be87c3ea997e
Zachary Hunter <zachary-hunter-design@gmail.com>
2021-03-25 13:06:52 -0600

    trying this

--- Starwars/starwars.js
+++ Starwars/starwars.js
@@ -2,4 +2,8 @@ import { films } from '../data/films.js'
 
 const greeting = "Hello World"
 
-console.log(films)
+console.log(films[1])
+
+let filmOne = document.querySelector("#film1")
+
+filmOne.textContent = films[0].title
