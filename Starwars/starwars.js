60dfdcbfef588a87644281b129949a81e5e1a602
Zachary Hunter <zachary-hunter-design@gmail.com>
2021-03-25 18:10:25 -0600

    styling

--- Starwars/starwars.js
+++ Starwars/starwars.js
@@ -8,9 +8,16 @@ for (let i = 0; i < films.length; i++) {
 
     //let lastNum = getLastNumber(films[i].url)
     const foundFilm = films.find(film => getLastNumber(film.url) === (i + 1).toString())
-    let filmItem = document.createElement('li')
-    filmItem.textContent = films[i].title
-    filmList.appendChild(filmItem)
+    let posterFig = document.createElement('figure')
+    let figImg = document.createElement('img')
+    figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
+    let figCaption = document.createElement('figcaption')
+
+    figCaption.textContent = foundFilm.title
+    posterFig.appendChild(figImg)
+    posterFig.appendChild(figCaption)
+
+    filmList.appendChild(posterFig)
 }
 
 function getLastNumber(url) {
